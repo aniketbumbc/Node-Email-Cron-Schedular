68c7ef9eb3b9f69a3f1dea525a1a91e85d85a90b
aniket.bhavsar <aniket.bhavsar@accionlabs.com>
2020-05-08 14:23:48 +0530

    email object configure

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ const express = require("express");
 const nodeMailer = require("nodemailer");
 let testAccount = nodeMailer.createTestAccount();
 
-cron.schedule("* * * * *", function () {
-      console.log("Running Cron Job");
-      })
+cron.schedule("* * * * *", function() {
+  console.log("Running Cron Job");
+});
 
+let transponder = nodeMailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "jayne90@ethereal.email",
+    pass: "ppftSy1aQcTssGphUC"
+  }
+});
+
+const mailOptations = {
+  from: ' "Bunny Coder" <bunny.coder@example.com>', // sender address
+  to: "jayne90@ethereal.email", // list of receivers
+  subject: "Hello there!", // Subject line
+  text: "A Message from Node Cron App", // plain text body
+  html: "<b>A Message from Node Cron App</b>" // html body
+};
+
+transponder.sendMail(mailOptations, function(err, info) {
+      console.log(info.messageId);
+});
 
 app = express();
 app.listen(8000);
