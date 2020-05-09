adba67b37f7857c49ad62711a550a9208bc4172e
aniket.bhavsar <aniket.bhavsar@accionlabs.com>
2020-05-09 17:39:25 +0530

    modify index js

--- index.js
+++ index.js
@@ -18,14 +18,17 @@ let transponder = nodeMailer.createTransport({
 
 const mailOptations = {
   from: ' "Bunny Coder" <bunny.coder@example.com>', // sender address
-  to: "jayne90@ethereal.email", // list of receivers
+  to: "aniket.umbc@gmail.com", // list of receivers
   subject: "Hello there!", // Subject line
   text: "A Message from Node Cron App", // plain text body
-  html: "<b>A Message from Node Cron App</b>" // html body
+  html: "<h1>A Message from Node Cron App</h1>" // html body
 };
 
 transponder.sendMail(mailOptations, function(err, info) {
-      console.log(info.messageId);
+  console.log(info.messageId);
+  if (err) {
+    console.log(err);
+  }
 });
 
 app = express();
